6c6803f88601967a79fac5de08a07533d7bd2731
Steven Atkinson <mrstebouk@gmail.com>
2017-04-22 09:14:21 +0100

    Added Address module.

---
+++ src/modules/address/components/AddressPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AddressPage extends Component {
+  render() {
+    return (
+      <h1>Address</h1>
+    );
+  }
+}
+
+export default AddressPage;
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Router, Route } from 'react-router';
 
 import App from './components/App';
 import NotFound from './components/NotFound';
+import AddressModule from './modules/address';
 
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={App}>
+      <Route path="/address" component={AddressModule.Components.AddressPage} />
       <Route path="*" component={NotFound} />
     </Route>
   </Router>
